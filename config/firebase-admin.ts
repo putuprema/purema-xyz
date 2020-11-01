960041b27c6ecad44f0683ed27a116d76a348aa6
putuprema <iputupremaananda@gmail.com>
2020-11-02 00:48:35 +0700

    move database url to environment variables instead of hard coded

--- config/firebase-admin.ts
+++ config/firebase-admin.ts
@@ -2,7 +2,7 @@ import firebaseAdmin from "firebase-admin";
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
-    databaseURL: "https://purema-xyz.firebaseio.com",
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
     credential: firebaseAdmin.credential.cert({
       projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
       clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
